b9e745a43705c72803b10088ad8776acafe2e9e1
Aditya Kavdikar <aditya.kavdikar@yash.com>
2020-04-02 09:33:36 +0530

    DotNetPractice sln  and CsharpBasics project added

---
+++ DotNetPractice.CSharpBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetPractice.CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
